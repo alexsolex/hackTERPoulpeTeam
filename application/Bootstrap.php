cefd76a26a47b369b89680c660f4b35a91d30c44
alexsolex <alexsolex@gmail.com>
2013-11-18 22:09:49 +0100

    installation de l'identification par réseaux sociaux. Fichier application/configs/social.ini à configurer

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -11,6 +11,11 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
          
     }
 
+    protected function _initConfig()
+    {
+        Zend_Registry::set('config', $this->getOptions());
+    }
+    
     protected function _initAppAutoload()
     {
         $autoloader = new Zend_Application_Module_Autoloader(array(
@@ -25,6 +30,40 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $this->bootstrap('frontController');
         $layout = Zend_Controller_Action_HelperBroker::addHelper(
             new Application_View_Helper_LayoutLoader());
+    }
+    
+    protected function _initTimezone()
+    {
+        date_default_timezone_set('Europe/Paris');
+    }
+
+    protected function _initRoutes()
+    {
+        $front = Zend_Controller_Front::getInstance();
+        $router = $front->getRouter();
+
+        $route = new Zend_Controller_Router_Route('login/:provider',
+                                                  array(
+                                                  'module' => 'mobi',
+                                                  'controller' => 'user',
+                                                  'action' => 'login'
+                                                  ));
+        $router->addRoute('login/:provider', $route);
+         $route = new Zend_Controller_Router_Route_Static('login',
+                                                         array(
+                                                             'module' => 'mobi',
+                                                         'controller' => 'user',
+                                                         'action' => 'login'
+                                                         ));
+        $router->addRoute('login', $route);
+
+        $route = new Zend_Controller_Router_Route_Static('logout',
+                                                         array(
+                                                             'module' => 'mobi',
+                                                         'controller' => 'user',
+                                                         'action' => 'logout'
+                                                         ));
+        $router->addRoute('logout', $route);
     }
 }
 
