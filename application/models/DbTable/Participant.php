c0a7d9716dc37533ae4f2584547dcecdea90eebc
Fredo <fredo.ndm@gmail.com>
2013-11-19 18:10:09 +0100

    Mise en place de l'ihm des départs et appel au webservice avec soap

--- application/models/DbTable/Participant.php
+++ application/models/DbTable/Participant.php
@@ -5,11 +5,6 @@ class Application_Model_DbTable_Participant extends Zend_Db_Table_Abstract
     protected $_rowClass = 'Application_Model_Row_ParticipantRow';
     protected $_rowsetClass = 'Application_Model_Rowset_ParticipantRowset';
     protected $_dependentTables = array('Application_Model_DbTable_Participer');
-    protected $_referenceMap    = array(
-        'gagner' => array(
-            'columns'           => 'idQuestion',
-            'refTableClass'     => 'Application_Model_DbTable_Question',
-            'refColumns'        => 'idQuestion'
-        ));
+   
 }
 
