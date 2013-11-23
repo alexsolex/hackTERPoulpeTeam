e945b1ce25d11a012ff7f59f10985176a88017b9
Fredo <fredo.ndm@gmail.com>
2013-11-23 17:05:45 +0100

    suite

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -43,24 +43,50 @@ class TestController extends Zend_Controller_Action
 //        $this->view->situer = $t->fetchAll()->toArray();
         
         // exemple wikipedia
-        
-                //$fql_query_url = 'http://fr.wikipedia.org/w/api.php?format=json&action=query&titles=Orchies&prop=revisions&rvprop=content';
-        
+//        
+//                $fql_query_url = 'http://fr.wikipedia.org/w/api.php?format=json&action=query&titles=Orchies&prop=revisions&rvprop=content';
+//        
 //
 //        $fql_query_result = file_get_contents($fql_query_url);
 //        $fql_query_obj = json_decode($fql_query_result, true);
 //        
 //        $this->view->json = $fql_query_obj;
 //    
-        $ville = 'Lille';
-	
-	$queryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' . $ville . '&units=metric&lang=fr';
+//        $ville = 'Lille';
+//	
+//	$queryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' . $ville . '&units=metric&lang=fr';
+//	
+//	$result = file_get_contents($queryUrl);
+//	$tabResult = json_decode($result, true);
+//        
+//        $this->view->result = $tabResult;
+
+        
+	//Recherche des villes, régions...en fonction des coordonnées
+	$queryUrl = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=50.650,3.083&sensor=true';
 	
 	$result = file_get_contents($queryUrl);
 	$tabResult = json_decode($result, true);
         
-        $this->view->result = $tabResult;
-
+        $t = new Application_Model_DbTable_Gare();
+        $tableauGare = $t->fetchAll();
+        
+        foreach ($tableauGare as $resRow ){
+            //modif des coordonnees
+            $xGare = $gareRow['Xcoord'];
+            $yGare = $gareRow['Ycoord'];
+            
+            $coordsGare = $geo->getLambert93VersWGS84($xGare, $yGare);           
+  
+            $queryUrl = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' . $coordsGare .'&sensor=true';
+            $result = file_get_contents($queryUrl);
+            $tabResult = json_decode($result, true);
+            
+            //Ajout des données dans la ligne de la table
+            
+        }
+        
+        
     }
     
 public function listerFacebook() {
