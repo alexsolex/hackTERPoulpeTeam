23e3be875764fa2fc4d5587f9aa975d326f02e92
alexsolex <alexsolex@gmail.com>
2013-11-23 19:05:54 +0100

    preparation de l'organisation de l'application mobile

--- application/modules/mobi/controllers/IndexController.php
+++ application/modules/mobi/controllers/IndexController.php
@@ -2,49 +2,52 @@
 
 class Mobi_IndexController extends Zend_Controller_Action {
 
+    const NO_AUTH_POSSIBLE = false;
+    
     public function init() {
         
         
     }
 
     public function indexAction() {
-        $idGare = $this->getIdGare();
-        $question =  $this->getQuestion($idGare);
-        $arrayQuestion = $question[1];
-        $idQuestion = $question[0];
-        $this->view->question = $arrayQuestion;
-        $leSponsor = $this->getSponsor($idQuestion);
-        $this->view->sponsor = $leSponsor[1];
+        //simple redirection pour l'url de base
+        if (self::NO_AUTH_POSSIBLE) {
+            
+        }
+        else {
+            $this->_redirect('/mobi/user/index');
+        }
+        
         
     }
 
     
-    public static function getQuestion($idGare) {
-        //TODO $idGare;
-        return array( 1, //id de la question
-            array (
-                'question' => "Une commune française, située dans le département du Nord (59) en région Nord-Pas-de-Calais. Le nom jeté des habitants est les pourchots1, signifiant « porc » en picard.",
-                'choix1' => "GareA",
-                'choix2' => "Orchies",
-                'choix3' => "GareC",
-                'choix4' => "GareD",
-            )
-        );
-    }
-
-    public static function getSponsor($idQuestion) {
-        return array( 1,
-            array (
-                'nom' => 'Starbuck',
-                'FB' => 'http://lienfacebook/starbuck',
-                'twitter' => null,
-                'google' => null,
-                'url' => 'http://www.starbuck.fr'
-                )
-            );
-    }
-
-    public static function getIdGare() {
-        return 12434;
-    }
+//    public static function getQuestion($idGare) {
+//        //TODO $idGare;
+//        return array( 1, //id de la question
+//            array (
+//                'question' => "Une commune française, située dans le département du Nord (59) en région Nord-Pas-de-Calais. Le nom jeté des habitants est les pourchots1, signifiant « porc » en picard.",
+//                'choix1' => "GareA",
+//                'choix2' => "Orchies",
+//                'choix3' => "GareC",
+//                'choix4' => "GareD",
+//            )
+//        );
+//    }
+//
+//    public static function getSponsor($idQuestion) {
+//        return array( 1,
+//            array (
+//                'nom' => 'Starbuck',
+//                'FB' => 'http://lienfacebook/starbuck',
+//                'twitter' => null,
+//                'google' => null,
+//                'url' => 'http://www.starbuck.fr'
+//                )
+//            );
+//    }
+//
+//    public static function getIdGare() {
+//        return 12434;
+//    }
 }
