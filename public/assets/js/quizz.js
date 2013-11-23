e1f7c9068dc7d41d615e5a854514ab8ed30c4c24
Laurent Soyris <Soyris@gmail.com>
2013-11-23 19:18:02 +0100

    JS coté client

--- public/assets/js/quizz.js
+++ public/assets/js/quizz.js
@@ -1,3 +1,5 @@
+alert('test');
+
 /* 
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -5,13 +7,17 @@
 
 var urlApiGeo = BASE_URL + '/api/user/geolocalise/';
 var urlApiQuestion = BASE_URL + '/api/question/obtenir/TVS/';
+var urlApiReponse = BASE_URL + '/api/question/repondre/';
+var TVS = '';
+var idQuestion = -1;
 
 function echecGeo()
 {
     alert('val stat !!');
     recupererGare(
-            50.650,
-            3.083
+
+            50.63654,
+            3.070366
             );
 }
 
@@ -26,27 +32,27 @@ function reussiteGeo(position)
 
 function recupererGare(lat, long)
 {
-    var questionJSON = {
-        "question": {
-            "question": "  une commune du nord de la France, pr\u00e9fecture du d\u00e9partement du Nord et chef-lieu en r\u00e9gion Nord-Pas-de-Calais. Surnomm\u00e9e la \u00ab Capitale des Flandres \u00bb une commune du nord de la France, pr\u00e9fecture du d\u00e9partement du Nord et chef-lieu en r\u00e9gion Nord-Pas-de-Calais. Surnomm\u00e9e la \u00ab Capitale des Flandres \u00bbune commune du nord de la France, pr\u00e9fecture du d\u00e9partement du Nord et chef-lieu en r\u00e9gion Nord-Pas-de-Calais. Surnomm\u00e9e la \u00ab Capitale des Flandres \u00bb une commune du nord de la France, pr\u00e9fecture du d\u00e9partement du Nord et chef-lieu en r\u00e9gion Nord-Pas-de-Calais. Surnomm\u00e9e la \u00ab Capitale des Flandres \u00bb",
-            "choix1": "Lille",
-            "choix2": "Orchies",
-            "choix3": "Simcity",
-            "choix4": "bruxelles"
-        },
-        "sponsor": {
-            "nom": "Starbuck",
-            "FB": null,
-            "twitter": null,
-            "google": null,
-            "url": "www.starbuck.Fr",
-            "logo": "Starbucks.png"
-        },
-        "tvs": "LLF",
-        "idq": "2"
-    };
-    jsonToView(questionJSON);
-    return;
+//    var questionJSON = {
+//        "question": {
+//            "question": "  une commune du nord de la France, pr\u00e9fecture du d\u00e9partement du Nord et chef-lieu en r\u00e9gion Nord-Pas-de-Calais. Surnomm\u00e9e la \u00ab Capitale des Flandres \u00bb une commune du nord de la France, pr\u00e9fecture du d\u00e9partement du Nord et chef-lieu en r\u00e9gion Nord-Pas-de-Calais. Surnomm\u00e9e la \u00ab Capitale des Flandres \u00bbune commune du nord de la France, pr\u00e9fecture du d\u00e9partement du Nord et chef-lieu en r\u00e9gion Nord-Pas-de-Calais. Surnomm\u00e9e la \u00ab Capitale des Flandres \u00bb une commune du nord de la France, pr\u00e9fecture du d\u00e9partement du Nord et chef-lieu en r\u00e9gion Nord-Pas-de-Calais. Surnomm\u00e9e la \u00ab Capitale des Flandres \u00bb",
+//            "choix1": "Lille",
+//            "choix2": "Orchies",
+//            "choix3": "Simcity",
+//            "choix4": "bruxelles"
+//        },
+//        "sponsor": {
+//            "nom": "Starbuck",
+//            "FB": null,
+//            "twitter": null,
+//            "google": null,
+//            "url": "www.starbuck.Fr",
+//            "logo": "Starbucks.png"
+//        },
+//        "tvs": "LLF",
+//        "idq": "2"
+//    };
+//    jsonToView(questionJSON);
+//    return;
     chargerJSON(
             urlApiGeo + 'lat/' + lat + '/long/' + long,
             {},
@@ -64,29 +70,56 @@ function jsonToView(data) {
     $('#reponse2').text(data.question.choix2);
     $('#reponse3').text(data.question.choix3);
     $('#reponse4').text(data.question.choix4);
+    idQuestion = data.idq;
+}
+
+function clickReponse(){
+    var $item = $(this);
+    var reponse = $item.text();
+    chargerJSON(
+        urlApiReponse + 'TVS/' + TVS + '/idq/' + idQuestion + '/reponse/' + reponse,
+        {},
+        reponseEnvoyee,
+        erreurReponse
+        );
+    return false;
+}
+
+function reponseEnvoyee (data, textStatus, jqXHR){
+    var reponseOK = data.reponseOK;
+    if (reponseOK) {
+        $('.question')
+                .css('display', 'none')
+                .parent()
+                .text('bravo');
+        
+    }
+}
+
+function erreurReponse() {
     
 }
 
-
 function getLocation()
 {
-    if (navigator.geolocation)
-    {
-        navigator.geolocation.getCurrentPosition(reussiteGeo);
-        // Fourni position.coords.latitude
-        // et position.coords.longitude
-    }
-    else
-    {
+//    if (navigator.geolocation)
+//    {
+//        navigator.geolocation.getCurrentPosition(reussiteGeo);
+//        // Fourni position.coords.latitude
+//        // et position.coords.longitude
+//    }
+//    else
+//    {
         echecGeo();
-    }
+//    }
 }
 
 function recupererQuestion(data, textStatus, jqXHR) {
     var gareJson = data;
 
+    TVS = gareJson.gare.tvs;
     chargerJSON(
-            urlApiQuestion + 'LLF', //gareJson.gare.tvs,
+            urlApiQuestion + gareJson.gare.tvs,
             {},
             afficherQuestion,
             function() {
@@ -98,10 +131,8 @@ function recupererQuestion(data, textStatus, jqXHR) {
 
 function afficherQuestion(data, textStatus, jqXHR) {
     var questionJson = data;
-    var question = question.question;
-    alert(question);
-
-    alert('Ok :)');
+    var question = questionJson.question;
+    jsonToView(questionJson);
 }
 
 
@@ -128,4 +159,7 @@ function chargerJSON(url, data, ftSuccess, ftError) {
     //jQuery.getJSON(options.url, options.data, ftSuccess);//ne permet pas les POST
     jQuery.ajax(options);
 }
+
+$('.bouton-reponse').click(clickReponse);
+
 $(getLocation);
